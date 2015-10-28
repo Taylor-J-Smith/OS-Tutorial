e011f879379e201060c804d2e555f6bb82d19f56
Stewart <stewart.gibson@uoit.net>
2015-10-28 13:14:18 -0400

    Question 2 complete

--- Tutorial6/A-Question2.c
+++ Tutorial6/A-Question2.c
@@ -6,5 +6,25 @@
 
 int main(void)
 {
-  return 0;
+	int PID = 0;
+
+	PID = fork();
+
+	if(PID==-1){
+		printf("Error forking.\n");
+		return 0;
+	}
+
+	if(PID==0){
+		sleep(1);
+		printf("Child process.\n");
+
+		exit(0);
+	} else {
+		int status;
+		wait(&status);
+		printf("Parent process.\nReturn status: %d\n",status);
+	}
+
+	return 0;
 }
