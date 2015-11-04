6e3a1d3180cb858595228006ac6058e99b315436
alex <uwalex@gmail.com>
2015-11-04 12:51:48 -0500

    Q4: completed and done

--- Tutorial7/A-Question4.c
+++ Tutorial7/A-Question4.c
@@ -7,7 +7,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main(void){
+int main(void){    
+  pid_t pid = fork();
+  if (pid == 0){
+    //child process
+    puts("entering child:");
+    execlp("./process",NULL);
+    exit(0);
+  }else if (pid > 0){
+    //parent process
+    puts("parent process...waiting 5 sec...");
+    sleep(5);
+    puts("[parent] Sending SIGTSTP...");
+    kill(pid,SIGTSTP);
+    puts("[parent] Sleeping for 10sec...");
+    sleep(10);
+    puts("[parent] Sending SIGCONT...");
+    kill(pid,SIGCONT);
+    //    wait(NULL);
+    waitpid(pid,0,0);
+    puts("[parent] Parent Exiting!");
+  }else{
+    //fork failed
+  }
 
-	return 0;
+  
+  return 0;
 }
