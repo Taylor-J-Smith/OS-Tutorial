324e39aaa738528c54ba8fda6b3c44e5f4f0c51d
alex <uwalex@gmail.com>
2015-11-08 21:28:41 -0500

    fixing questions 1 and 2, were not properly reading values from file

--- Tutorial7/A-Question1.c
+++ Tutorial7/A-Question1.c
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+char** tokenize2(char *input, char *delim);
+
 typedef struct{
 	char name[256]; // character array of 256 length 
 	int priority; // integer for the process priority 
@@ -77,12 +79,13 @@ void readFile(){
 	for(int i=0; i<10; i++){
 		fgets(line,256,fp);
 		for(int j=0;j<256;j++){process->name[j] = 0;} // zero char array
-		strcpy(process->name, strtok(line, ",")); // copy name from file into process
-		// assign integer values from file
-		process->priority = (int) (strtok(NULL, ",")-'0');
-		process->pid = (int) (strtok(NULL, ",")-'0');
-		process->runtime = (int) (strtok(NULL, "\n")-'0');
-
+		char **tokenized = tokenize2(line,", ");
+		strcpy(process->name, tokenized[0]);
+		// assign integer values from file		
+		process->priority = atoi(tokenized[1]);
+		process->pid = atoi(tokenized[2]);
+		process->runtime = atoi(tokenized[3]);
+		
 		// push process onto queue
 		push(*process);
 
@@ -93,4 +96,47 @@ void readFile(){
 	}
 
 	fclose(fp);
+}
+
+char** tokenize2(char *input, char *delim){
+  //takes an input string with some delimiter and returns an array
+  //with all the tokens split by the provided delimiter
+
+  //Sample usage:
+  //char buffer[] = "a b c";
+  //char **user_output;
+  //user_output = tokenize(buffer, " ");
+
+  char** tokens = 0;
+  size_t num_elements = 0;
+  size_t tokens_index  = 0; //keep tracks of the  tokens offset when adding them
+  char* input_cpy = input;
+  char *input_cpy2 = malloc(1 + strlen(input)); //used with strtok
+  if (input_cpy2){
+    strcpy(input_cpy2, input);
+  }else{
+    printf("error copying input\n");
+  }
+
+  //iterate through the intput and count # of delims
+  while (*input_cpy != NULL){
+    if (*delim == *input_cpy){
+      num_elements++;
+    }input_cpy++;
+  }
+
+  num_elements++; //for last object
+  num_elements++; //for null terminating value
+
+  //create enough memory for all the elements
+  tokens = malloc(sizeof(char*) * num_elements);
+  char* token = strtok(input_cpy2, delim);
+  while (token){
+    //store the token in the tokens array
+    *(tokens + tokens_index++) = strdup(token);   //strdup duplicates the string
+    token = strtok(0, delim); //next token
+  }
+  //finally add null value at the end
+  *(tokens + tokens_index) = 0;
+  return tokens;
 }
