daad096d86f1892208c6dad69c3e492b3980e742
Stewart <stewart.gibson@uoit.net>
2015-11-11 11:42:32 -0500

    A-Question1.c structs added

--- Tutorial8/A-Question1.c
+++ Tutorial8/A-Question1.c
@@ -7,6 +7,21 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define CHAR_LENGTH 256
+
+typedef struct{
+	char parent[CHAR_LENGTH];
+	char name[CHAR_LENGTH];
+	int priority;
+	int memory;
+} proc;
+
+typedef struct tree{
+	proc process;
+	struct tree *left;
+	struct tree *right;
+} proc_tree;
+
 int main(void){
 
 }
