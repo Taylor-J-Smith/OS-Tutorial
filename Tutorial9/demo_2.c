b5e3db545dc651c3c8b064b42833a4ce1b7e0b95
jerkwok <jeremy.kwok@uoit.net>
2015-11-18 12:28:39 -0500

    added comments for question 1

--- Tutorial9/demo_2.c
+++ Tutorial9/demo_2.c
@@ -3,12 +3,17 @@
 int main (int argc, char *argv[])
 {
     int thread_num, nthreads = 16;
+
+    //The statements in this ifdef will only be executed if the compiler supports OpenMP
     #ifdef _OPENMP
     omp_set_num_threads(nthreads);
     #endif
+
     printf("Testing OpenMP, you should see each thread print...\n");
+    //This pragma statement parallizes the statements inside this block.
     #pragma omp parallel
     {
+        //This pragma statement states that the code inside this block is a critical section.
         #pragma omp critical
         {
           #ifdef _OPENMP
