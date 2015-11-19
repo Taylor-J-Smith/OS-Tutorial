fcf65cbf60590d1b2d1add40a2a28bd6138dd8ea
Stewart <stewart.gibson@uoit.net>
2015-11-19 16:34:11 -0500

    A-Question5.c pre-debugging

--- Tutorial9/A-Question5.c
+++ Tutorial9/A-Question5.c
@@ -3,7 +3,31 @@
 #include <math.h>
 #include <omp.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int nthreads = argc;
+	int n = 100;//000000;
+	double dx = 1/(n+1);
+	double x = 0;
+    #ifdef _OPENMP
+    omp_set_num_threads(nthreads);
+    #endif
+    #pragma omp parallel for private(x)
+    for(int i=0; i<n; i++){
+    	x = i*dx;
+    	double y = exp(x)*cos(x)*sin(x)*sqrt(5*x+6.0);
+    	if(1){
+        	#pragma omp critical
+        	{
+         	 #ifdef _OPENMP
+        		FILE *fp = fopen("calculations.txt","w");
 
+        		fprintf(fp, "%d %5.7f %5.7f", i, x, y);
+
+        		fclose(fp);
+          	#endif
+        	}
+        }
+    }
+    return 0;
 }
